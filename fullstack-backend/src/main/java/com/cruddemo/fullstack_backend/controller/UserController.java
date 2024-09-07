0ede541a8dd23339738c52a74e2b4c6ad32cf6bb
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-09-06 21:16:17 -0400

    Post Data into Database

--- fullstack-backend/src/main/java/com/cruddemo/fullstack_backend/controller/UserController.java
+++ fullstack-backend/src/main/java/com/cruddemo/fullstack_backend/controller/UserController.java
@@ -1,4 +1,20 @@
 package com.cruddemo.fullstack_backend.controller;
 
+import com.cruddemo.fullstack_backend.model.User;
+import com.cruddemo.fullstack_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
 }
